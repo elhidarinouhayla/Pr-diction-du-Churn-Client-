8b384c1fcaebe735fb28b5f0e1403994dea78a84
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-10-08 22:53:36 +0100

    modelisation

--- pipline.py
+++ pipline.py
@@ -3,6 +3,11 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report
+
 
 
 
@@ -10,11 +15,10 @@ from sklearn.model_selection import train_test_split
 def charge_donnees():
     file = pd.read_csv("telecom_churn.csv")
     return file
-data= charge_donnees("telecom_churn.csv")
+data= charge_donnees()
 
-def transforme_variable(df):
+def pretraitement_variable(df):
     df = charge_donnees()
-    list_columns=[]
 # 1- Convertir la colonne 'TotalCharges' en numérique
     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors= 'coerce')
 
@@ -27,24 +31,38 @@ def transforme_variable(df):
     df = df.drop(columns=['customerID', 'gender'])
 
 # 4- Encoder les variables catégorielles
+    list_columns = []
     for col in df.columns:
         if df[col].dtypes == 'object':
-          list_columns.append(col)
+            list_columns.append(col)
     print(list_columns)
-    df['list_columns'] = le.fit_transform(df['list_columns'])
+    df[col] = le.fit_transform(df[col])
+    return df
 
-nettoyage = transforme_variable(data)
+data = pretraitement_variable(data)
 
 
 def split_features(df):
     
-    x = df.drop('Churn')
+# 1- separation de features et la cible(churn)
+    x = df.drop('Churn', axis=1)
     y = df['Churn']
-    x_train,x_test,y_train,y_test = train_test_split(x,y ,test_size=0.2)
-    print("x_train: ", x_train)
-    print("x_test: ", x_test)
-    print("y_train: ", y_train)
-    print("y_test:", y_test)
+    return x, y
+
+def split_data(x,y, test_size=0.2):
+
+# 1- split_train_test 
+    x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2)
+    return  x_train,x_test,y_train,y_test
+
+# def scaler_data(x_test,x_train):
+#     x_train_scaled = scaler.fit_transform(x_train)
+#     x_test_scaled = scaler.transform(x_test)
+#     return x_train_scaled, x_test_scaled
+
+
+
+
 
 
 
