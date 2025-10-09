d00b0065ca2412dd39f34ae4446b567f74a9ba55
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-10-09 13:00:22 +0100

    traitement du model

--- pipline.py
+++ pipline.py
@@ -7,6 +7,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import MinMaxScaler
 
 
 
@@ -16,6 +17,7 @@ def charge_donnees():
     file = pd.read_csv("telecom_churn.csv")
     return file
 data= charge_donnees()
+
 
 def pretraitement_variable(df):
     df = charge_donnees()
@@ -31,15 +33,27 @@ def pretraitement_variable(df):
     df = df.drop(columns=['customerID', 'gender'])
 
 # 4- Encoder les variables catégorielles
-    list_columns = []
-    for col in df.columns:
-        if df[col].dtypes == 'object':
-            list_columns.append(col)
-    print(list_columns)
-    df[col] = le.fit_transform(df[col])
+    # list_columns = []
+    # for col in df.columns:
+    #     if df[col].dtypes == 'object':
+    #         list_columns.append(col)
+    categoric_cols = [
+        'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 
+        'InternetService', 'OnlineSecurity', 'OnlineBackup', 
+        'DeviceProtection', 'TechSupport', 'StreamingTV', 
+        'StreamingMovies', 'Contract', 'PaperlessBilling', 
+        'PaymentMethod', 'Churn']
+    
+    for col in categoric_cols:
+        df[col] = le.fit_transform(df[col])
     return df
+data_clean = pretraitement_variable(data)
 
-data = pretraitement_variable(data)
+
+numeric_cols =data[['SeniorCitizen', 'tenure', 'MonthlyCharges', 'TotalCharges']]
+clean_colomns = pd.concat([data_clean,numeric_cols], axis=0)
+print(clean_colomns)
+
 
 
 def split_features(df):
@@ -49,16 +63,25 @@ def split_features(df):
     y = df['Churn']
     return x, y
 
-def split_data(x,y, test_size=0.2):
+
+def split_data(x,y):
 
 # 1- split_train_test 
     x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2)
     return  x_train,x_test,y_train,y_test
 
-# def scaler_data(x_test,x_train):
-#     x_train_scaled = scaler.fit_transform(x_train)
-#     x_test_scaled = scaler.transform(x_test)
-#     return x_train_scaled, x_test_scaled
+
+def normalized_data(x_test,x_train):
+
+# 1- normalisation des variables numerique
+    scaler = MinMaxScaler()
+    x_test[clean_colomns] = scaler.fit_transform(x_test[clean_colomns])
+    x_train[clean_colomns] = scaler.fit_transform(x_train[clean_colomns])
+    return x_test,x_train
+
+
+
+
 
 
 
